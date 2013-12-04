9e7db4168e490cb82ba48dc09b249a52461c6f52
Murtaza Husain <murtaza.husain@thoughtworks.com>
2013-12-04 23:45:38 +0530

    initial commit

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject clj-oauth2 "0.1.0"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/murtaza52/clj-oauth2"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/murtaza52/clj-oauth2"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http-kit "2.1.13"]
                  [base64-clj "0.1.1"]
